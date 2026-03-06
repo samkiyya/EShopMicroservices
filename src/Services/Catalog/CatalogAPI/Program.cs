e1f98b6fe6e076e42f42f0a0dd747116c9be104b
samkiyya <samuelkiyya2012@gmail.com>
2026-03-06 10:52:17 +0300

    create catalog api project

---
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -0,0 +1,21 @@
+// Creates a WebApplicationBuilder instance.
+// This sets up the application's configuration, logging, and dependency injection container.
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+// Add/Register services with the dependency injection container.
+// This is where we add services like controllers, database contexts, authentication, etc.
+
+
+// Builds the WebApplication instance using the configured builder.
+// After this step, the application's middleware pipeline can be configured.
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// Middleware components (like routing, authentication, error handling, etc.) are added here
+// to process incoming HTTP requests and outgoing responses.
+
+
+// Starts the application and begins listening for incoming HTTP requests.
+app.Run();
