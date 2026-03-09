f995e1c77d0c05009398ddbae9d24d06a3b20b9b
samkiyya <samuelkiyya2012@gmail.com>
2026-03-09 16:13:37 +0300

    create CQRS (Command Query Responsibility Segregation ) command and query placeholder

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreatProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreatProductHandler.cs
@@ -1,6 +1,16 @@
-﻿namespace CatalogAPI.Products.CreateProduct
+﻿using MediatR;
+
+namespace CatalogAPI.Products.CreateProduct
 {
-    public class CreatProductHandler
+
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
     {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            // business logic to create a product
+            throw new NotImplementedException();
+        }
     }
 }
--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,8 @@
 ﻿namespace CatalogAPI.Products.CreateProduct
 {
+
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record CreateProductResult(Guid Id);
     public class CreateProductEndpoint
     {
     }
