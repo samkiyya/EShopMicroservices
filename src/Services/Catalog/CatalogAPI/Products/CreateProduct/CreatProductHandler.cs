311055b8e042c6588d3e2aa9b6b9dd308691ae2b
samkiyya <samuelkiyya2012@gmail.com>
2026-03-10 15:29:37 +0300

    referencing CQRS building blocks in catalog service

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreatProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreatProductHandler.cs
@@ -1,16 +1,35 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
 
 namespace CatalogAPI.Products.CreateProduct
 {
 
-    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : IRequest<CreateProductResult>;
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
             // business logic to create a product
-            throw new NotImplementedException();
+
+            //create product entity from incomming command object
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Description = command.Description,
+                Category = command.Category,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+
+            //TODO
+            //save to database
+
+
+
+            //return a response (CreateProductResult)
+            return new CreateProductResult(Guid.NewGuid());
         }
     }
 }
