73147c25165e59e0aeb7290ee15609abf4103bd8
samkiyya <samuelkiyya2012@gmail.com>
2026-03-16 09:39:43 +0300

    Register Carter Library into ASP.NET Dependency Injection Service

--- src/Services/Catalog/CatalogAPI/Program.cs
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add/Register services with the dependency injection container.
 // This is where we add services like controllers, database contexts, authentication, etc.
-
+builder.Services.AddCarter();
 
 // Builds the WebApplication instance using the configured builder.
 // After this step, the application's middleware pipeline can be configured.
@@ -15,7 +15,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 // Middleware components (like routing, authentication, error handling, etc.) are added here
 // to process incoming HTTP requests and outgoing responses.
-
+app.MapCarter();
 
 // Starts the application and begins listening for incoming HTTP requests.
 app.Run();
